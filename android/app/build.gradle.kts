aa1700d74c0d20ed8d0088c962f17b61d1c1cbed
ZephYer8 <zhangqiusheng977@gmail.com>
2026-06-02 21:24:14 +0800

    Bump Android app to 0.6.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.modeltest.monitor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.6.7"
+        versionCode = 15
+        versionName = "0.6.8"
         manifestPlaceholders["usesCleartextTraffic"] = "true"
     }
 
