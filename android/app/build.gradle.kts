7af087c064aa96723674947116c70a1c8235de4f
ZephYer8 <zhangqiusheng977@gmail.com>
2026-06-02 20:41:25 +0800

    Bump Android app to 0.6.6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.modeltest.monitor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.6.5"
+        versionCode = 13
+        versionName = "0.6.6"
     }
 
     buildFeatures {
